01e209ef33b99ea2a4fe3c78c0c2c5c7406466ce
varun sonkar <varunsonkar03@gmail.com>
2023-07-25 22:00:09 +0530

    adding B.java

---
+++ B.java
@@ -0,0 +1,10 @@
+
+public class B {
+	public static void main(String args[])   
+	{  
+	int n1 = 200, n2 = 100, minus;  
+	minus = n1 - n2;  
+	System.out.println("The minus : "+minus);  
+	}  
+
+}
