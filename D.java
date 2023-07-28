5042a10889a12b4ef9fd17a4ae9a4dbec6d1248f
varun sonkar <varunsonkar03@gmail.com>
2023-07-28 12:25:03 +0530

    D.java file

---
+++ D.java
@@ -0,0 +1,10 @@
+
+public class D {
+	public static void main(String args[])   
+	{  
+	int n1 = 200, n2 = 100, minus;  
+	minus = n1 - n2;  
+	System.out.println("The minus : "+minus);  
+	}  
+
+}
