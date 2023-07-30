aa8d24f484b0ebe93e88655606334b777ac72849
varunsss <varunsonkar03@gmail.com>
2023-07-30 17:18:48 +0530

    Update D.java
    added by varun on 30-july-2023

--- D.java
+++ D.java
@@ -3,7 +3,7 @@ public class D {
 	public static void main(String args[])   
 	{  
 	 
-	System.out.println("changes on 30-07-2023");  
+	System.out.println("changes on 30-07-2023  !!!!");  
 	}  
 
 }
